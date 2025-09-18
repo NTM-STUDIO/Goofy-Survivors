ce7aff461d8ca52b437f0cbb3521a39790a290a4
GONCAVF <veloso.goncalo@gmail.com>
2025-09-18 15:40:03 +0100

    Simple player movement

---
+++ Assets/Movement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 1f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * speed;
+    }
+}
