2801dc9b115f49556b5efeda1b3ed51e3d0602d4
GONCAVF <veloso.goncalo@gmail.com>
2025-09-26 16:25:10 +0100

    wave able to handle multiple enemy types & Add WaveEnemy to hold each enemy stats

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -2,10 +2,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
+public class WaveEnemy
+{
+    public GameObject enemyPrefab; // Prefab of the enemy to spawn
+    public int enemyCount; // Number of this enemy to spawn
+}
+
+[System.Serializable]
 public class Wave
 {
-    public string waveName;
-    public GameObject enemyPrefab;
-    public int enemyCount;
-    public float spawnInterval;
+    public string waveName; // Name of the wave (optional)
+    public List<WaveEnemy> enemies; // List of enemy groups in this wave
+    public float spawnInterval; // Time between spawns in seconds
+    public float healthMultiplierPerWave = 0.1f; // Health increase multiplier per wave 
 }
