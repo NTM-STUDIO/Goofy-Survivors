ba2e173fdf6e1fe765f02154e0731916fa291f88
Nuno Ribeiro <nunoribeiro2002@hotmail.com>
2025-10-10 19:44:12 +0100

    Revert "Teoria do nuno que eu dar comit vai resolver o problema dele"
    This reverts commit 8b7b39cc36a86962dd1ce4e69c3af74aab84f412.

--- Assets/!Scripts/Enemy/EnemyProjectileDamage.cs
+++ Assets/!Scripts/Enemy/EnemyProjectileDamage.cs
@@ -10,17 +10,14 @@ public class EnemyProjectileDamage : MonoBehaviour
     public EnemyStats enemyStats;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Projectile collided with: " + other.name);
         // S� aplica dano se colidir com o player
         if (other.CompareTag("Player"))
         {
             var playerStats = other.GetComponent<PlayerStats>();
             if (playerStats != null)
             {
-                playerStats.ApplyDamage(damageAmount, transform.position, 0f);
+                playerStats.ApplyDamage(damageAmount, transform.position, 0f); 
             }
-            
-            Destroy(gameObject);
         }
     }
 }
